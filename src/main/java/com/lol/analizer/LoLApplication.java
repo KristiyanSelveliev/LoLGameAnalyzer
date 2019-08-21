d7ac123645e6e23a7f0b6c53cf1200bfe92578f7
KristiyanSelveliev <kriselveliev@abv.bg>
2019-08-21 22:27:42 +0300

    First commit

---
+++ src/main/java/com/lol/analizer/LoLApplication.java
@@ -0,0 +1,13 @@
+package com.lol.analizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoLApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoLApplication.class, args);
+	}
+
+}
